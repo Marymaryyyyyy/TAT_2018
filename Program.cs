daaffdf5a7bc0cb8639a634b7f064c75cb21b5d1
Gruzinova Maria <34262218+Marymaryyyyyy@users.noreply.github.com>
2018-03-02 13:51:00 +0300

    Add file

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AboutMe
+{
+
+    class AboutMe
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("My name is Mariya Hruzinava. I'm studying at BSU on 3rd year.");
+            Console.ReadKey();
+        }
+    }
+}
