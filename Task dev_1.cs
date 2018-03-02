829ee774f4e8b5add3caadd1960fcc8c7688f1ae
Gruzinova Maria <34262218+Marymaryyyyyy@users.noreply.github.com>
2018-03-02 17:04:26 +0300

    Add programm files

---
+++ Task dev_1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_dev_1
+{
+    /// <summary>
+    /// This is the input point of the program.
+    /// </summary>
+
+    class EntryPoint
+    {
+        /// <summary>
+        /// This is the input point of the program.
+        /// </summary>
+        /// <param name="args">String to input</param>
+        static void Main(string[] args)
+        {
+            repeatingSymbols inputString = new repeatingSymbols(args[0]);
+            Console.WriteLine(inputString.MaxNumberrepeatingSymbols());
+            Console.ReadKey();
+        }
+    }
+   
+}
+
