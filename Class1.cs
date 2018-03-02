6a1c0a7367d9c40e4cd9981dd126ea8f3b6c7814
Gruzinova Maria <34262218+Marymaryyyyyy@users.noreply.github.com>
2018-03-02 23:15:38 +0300

    Add files via upload
    Removed unnecessary lines, renamed some elements.

---
+++ Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task_dev_1
+{
+    /// <summary>
+    /// This class is looking for repeating symbols in a string.
+    /// </summary>
+  
+    class RepeatingSymbols
+    {
+        private string inputString;
+
+        public RepeatingSymbols(string args)
+        {
+            inputString = args;
+        }
+        /// <summary>
+        /// This method finds the maximum number of identical symbols in the string
+        /// </summary>
+        /// <returns>This method prints the maximum number of identical symbols in the string</returns>
+        public int MaxNumberRepeatingSymbols()
+        {
+            int currentCount = 1;
+            int maxCount = 1;
+
+            for (int i = 0; i < inputString.Length - 1; i++)
+            {
+                if (inputString[i].Equals(inputString[i + 1]))
+                {
+                    currentCount++;
+                }
+                else
+                {
+                    if (currentCount > maxCount)
+                    {
+                        maxCount = currentCount;
+                    }
+                    currentCount = 1;
+                }
+             }
+            return maxCount;
+        }
+    }
+}
