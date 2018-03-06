f34e9b929eb973577445a134e57c67a0e6eeb0ab
Gruzinova Maria <34262218+Marymaryyyyyy@users.noreply.github.com>
2018-03-06 23:42:43 +0300

    Add files via upload
    Add files

---
+++ Converter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_dev_3
+{
+    /// <summary>
+    /// This class converts the number in other notation system.
+    /// </summary>
+    class Converter
+    {
+        private string convertedValue;            
+        
+        /// <summary>
+        /// This class converts the number in other notation system.
+        /// </summary>
+        /// <param name="number">input number</param>
+        /// <param name="convertedToValue">new number notation</param>
+        /// <returns>output number of the new notation system </returns>
+        public string ConvertTo(int number, int convertedToValue)
+        {
+            do
+            {
+                int rest = number % convertedToValue;
+                number = number / convertedToValue;
+                convertedValue = ChooseLetter(rest) + convertedValue;
+            }
+            while (number > 0);
+            return convertedValue;
+        }
+
+        private char ChooseLetter(int rest)
+        {
+            string elements = "0123456789ABCDEFGHIJ";
+            return elements[rest];
+        }
+    }
+}
---
+++ EntryPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_dev_3
+{   
+    /// <summary>
+    /// This is the input point of the program.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int convertedToValue;
+
+            if (args.Length > 2)
+            {
+                bool checkNumber = Int32.TryParse(args[0], out number);
+                bool checkFormatBase = Int32.TryParse(args[1], out convertedToValue);
+                bool checkIntervalBase = convertedToValue > 2 && convertedToValue < 20;
+
+                if (checkNumber && checkFormatBase && checkIntervalBase)
+                {
+                    Converter converter = new Converter();
+                    Console.WriteLine("Output: {0}", converter.ConvertTo(number, convertedToValue));
+                }
+                else
+                {
+                    Console.WriteLine("Error! Incorrect format of the input data!");
+                }
+            }
+            else
+                Console.WriteLine("Error! Incorrect format of the input data!");                 
+        }
+    }
+}
