256fb3f69cfd5dbddece25837ef65281eada4797
Gruzinova Maria <34262218+Marymaryyyyyy@users.noreply.github.com>
2018-03-23 15:54:02 +0300

    Task dev_6
    Add files

---
+++ Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_dev_6
+{
+    public class Product
+    {
+       public string Type { get; set; }
+       public string Name { get; set; }
+       public int Quantity { get; set; }
+       public decimal Cost { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,42 +1,99 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_dev_6.Interfaces;
+using Task_dev_6.Implementations;
 
-namespace Task_dev_3
+namespace Task_dev_6
 {
-    /// <summary>
-    /// This is the entry point of the program.
-    /// </summary>
     class EntryPoint
     {
         static void Main(string[] args)
         {
-            int inputNumber;
-            int convertedToValue;
+           var products = new List<Product>();
+           var key = new ConsoleKeyInfo();
 
-            if (args.Length < 2)
+            do
             {
-                Console.WriteLine("Error! Incorrect format of the input data!");
+                var product = new Product();
+
+              try { 
+               Console.WriteLine(" Input type of product");
+                product.Type = Console.ReadLine();
+
+                Console.WriteLine(" Input the name of product");
+                product.Name = Console.ReadLine();
+
+                Console.WriteLine(" Input the quontity");
+                product.Quantity = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(" Input the cost of one product");
+                product.Cost = Convert.ToDecimal(Console.ReadLine());
+
+                products.Add(product);
             }
-            else
+            catch(Exception e)
             {
-                try
-                {
-                    inputNumber = Int32.Parse(args[0]);
-                    convertedToValue = Int32.Parse(args[1]);
-                    if (convertedToValue > 1 && convertedToValue < 21)
-                    {
-                        Converter converter = new Converter();
-                        Console.WriteLine("Output: {0}", converter.ConvertTo(inputNumber, convertedToValue));
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error! Incorrect format of the input data!");
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e.Message);
-                }
+                Console.WriteLine(e.Message);
             }
+
+
+                Console.WriteLine("\n Do you want continue to input products?");
+               Console.WriteLine("Yes [A]|No[B]");
+               key = Console.ReadKey();
+           }
+           while (key.Key != ConsoleKey.B);
+
+            do
+            {
+                Console.WriteLine("\n Input command:" +                   
+                    "\n1) count types\n" +
+                    "2) count all\n" +
+                    "3) average price (all types)\n" +
+                    "4) average price (type)\n" +
+                    "5) exit" );
+                string data = Console.ReadLine();
+                if(data=="5")
+                {
+                    return;
+                }
+
+                SwitchCommand(products,data);
+
+                Console.WriteLine("\n Do you want continue to input commands?");
+                Console.WriteLine("Yes [A]| No[B]");
+                key = Console.ReadKey();
+            }
+            while (key.Key != ConsoleKey.B);
+
         }
+
+
+        static void SwitchCommand(List<Product> products, string data)
+        {           
+            switch (data)
+            {
+                case "1":
+                    Console.WriteLine(Invoker.Invoke(new CountTypes(), products, "count types"));
+                    break;
+                case "2":
+                    Console.WriteLine(Invoker.Invoke(new CountAll(), products, "count all"));
+                    break;
+                case "3":
+                    Console.WriteLine(Invoker.Invoke(new AveragePrice(), products, "average price"));
+                    break;
+                case "4":
+                    Console.WriteLine("Input type: ");
+                    var type = Console.ReadLine();
+                    Console.WriteLine(Invoker.Invoke(new AveragePriceType(), products, type));
+                    break;               
+                    
+             }
+            
+        }
+
+
     }
 }
